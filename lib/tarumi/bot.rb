58dedbb031ae9b0b3f343d3cb0668eed7d7ffb15
benji <benj_cous@hotmail.com>
2015-02-24 11:35:53 +0100

    Add possibility to add icon_url/icon_emoji in initialize

--- lib/tarumi/bot.rb
+++ lib/tarumi/bot.rb
@@ -4,8 +4,8 @@ module Tarumi
   class Bot
     include HTTParty
 
-    attr_reader :options, :username, :channel,
-                :hook_name
+    attr_reader :username, :channel, :hook_name,
+                :icon_emoji, :icon_url
 
     # Add the default format to be json on each request
     format :json
@@ -14,27 +14,40 @@ module Tarumi
     disable_rails_query_string_format if defined?(Rails)
 
     def initialize(team, token, options = {})
-      self.class.base_uri("https://#{team}.slack.com") 
+      self.class.base_uri("https://#{team}.slack.com")
       self.class.default_params(token: token) #we sent this token on each request
 
-      @username = options[:username] || "webhookbot"
-      @channel = options[:channel] || "#general"
-      @hook_name = options[:hook_name] || "incoming-webhook"
+      @username   = options[:username]   || "webhookbot"
+      @channel    = options[:channel]    || "#general"
+      @hook_name  = options[:hook_name]  || "incoming-webhook"
+      @icon_url   = options[:icon_url]   || "https://slack.com/img/icons/app-57.png"
+      @icon_emoji = options[:icon_emoji]
 
       raise ArgumentError, 'You need to specify a team' if team.blank?
       raise ArgumentError, 'You need to specify the token, to get one go to https://yourteamname.slack.com/services/new' if token.blank?
     end
 
     def ping(text, options = {})
-      payload = { text: text, channel: @channel, username: @username }.merge(options)
+      payload = {
+        text: text,
+        channel: @channel,
+        username: @username
+      }.merge(icon).merge(options)
+
       body = { body: "payload=#{payload.to_json}"}
 
-      response = self.class.post("/services/hooks/#{@hook_name}", body) 
+      response = self.class.post("/services/hooks/#{@hook_name}", body)
 
       raise ArgumentError, 'You need to specify a text' if text.blank?
 
       "#{response.body} - #{response.code}"
     end
+
+    private
+
+      def icon
+        @icon_emoji ? { icon_emoji: @icon_emoji } : { icon_url: @icon_url }
+      end
 
   end
 end
